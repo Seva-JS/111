413af8fb22ae605902028328fe810286f7da957b
seva <sevavac@gmail.com>
2021-02-24 17:43:06 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Serch from "./Main/Serch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+    constructor() {
+
+        super();
+        this.state = {
+            data: {},
+            load: false,
+            inputValue: ' '
+        }
+
+
+    }
+
+
+    request = () => {
+
+        let inputVal = document.getElementById("myInput").value;
+        if (inputVal != "") {
+            const api = 'd989440c836fbb9bdc5d22f6d5e7683f';
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${api}`
+            fetch(url, {
+                origin: 'cors'
+            }).then(resp => resp.json()).then(data => this.setState({
+                data: {data},
+                load: true,
+
+            }))
+        } else {
+            alert('Введите город !')
+        }
+
+
+    }
+
+    addWeatherToPage = () => {
+
+        if (this.state.load === true) {
+            let city = this.state.data.data.name
+            let temp = this.KtoC(this.state.data.data.main.temp);
+
+            return (
+                <div>
+                    <div> City: {city}</div>
+                    <div> Temperature: {temp}°</div>
+
+
+                </div>
+
+            )
+
+        }
+
+    }
+
+    KtoC = (K) => {
+        return Math.floor(K - 273.15);
+    }
+    inputValue = (e) => {
+        this.setState({
+            inputValue: e.currentTarget.value
+        })
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Serch
+                    addWeatherToPage={this.addWeatherToPage}
+                    request={this.request}
+                    inputValue={this.inputValue}
+                />
+            </div>
+        )
+
+    }
 }
-
-export default App;
