2cc50766f81183b1988c9fe96554ba88a50a285e
seva <sevavac@gmail.com>
2021-03-11 22:43:36 +0300

    Trying to make fetch req perfect

--- src/App.js
+++ src/App.js
@@ -1,104 +1,81 @@
 import React from "react";
 import Serch from "./Main/Serch";
 
-
 export default class App extends React.Component {
-    constructor() {
+  constructor() {
+    super();
+    this.state = {
+      data: {},
+      load: false,
+      inputValue: " ",
+    };
 
-        super();
-        this.state = {
-            data: {},
-            load: false,
-            inputValue: ' '
-        }
-
-
+    async function req (props) {
+      debugger;
+      try {
+       
+        let inputVal =props;
+        let cutVal = inputVal.trim();
+        const api = "d989440c836fbb9bdc5d22f6d5e7683f";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cutVal}&appid=${api}`;
+        let response = await fetch(url);
+        let data = await response.json(); 
+        console.log(data)
+      } catch (e) {
+        alert(e);
+      }
     }
+    this.req = req;
+  }
 
+  addWeatherToPage = () => {
+    if (this.state.load === true) {
+      let city = this.state.data.name;
+      let temp = this.KtoC(this.state.data.main.temp);
+      let minTemp = this.KtoC(this.state.data.main.temp_min);
+      let maxTemp = this.KtoC(this.state.data.main.temp_max);
+      let pressure = this.pressureCout(this.state.data.main.pressure);
 
-    request = () => {
-        try{
-
-        let inputVal = document.getElementById("myInput").value;
-        let cutVal=inputVal.trim();
-        if (cutVal !== "") {
-            const api = 'd989440c836fbb9bdc5d22f6d5e7683f';
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${api}`
-            fetch(url, {
-                origin: 'cors'
-            }).then(resp => resp.json()).then(data => this.setState({
-                data:data,
-                load: true,
-
-            }))
-        } else {
-            alert('Введите город !')
-        }
+      return (
+        <div>
+          <div> City: {city}</div>
+          <div>
+            {" "}
+            Temperature:
+            <ul>
+              <li>Now: {temp}°</li>
+              <li>Min: {minTemp}°</li>
+              <li>Max: {maxTemp}°</li>
+            </ul>
+            <div>Pressure: {pressure} mm Hg</div>
+          </div>
+        </div>
+      );
     }
-    catch(e){
-        alert(e); 
-        
+  };
+  pressureCout = (P) => {
+    return Math.floor(P / 1.3333);
+  };
 
-    }
+  KtoC = (K) => {
+    return Math.floor(K - 273.15);
+  };
+  inputValue = (e) => {
+    this.setState({
+      inputValue: e.currentTarget.value,
+    });
+  };
 
-
-    }
-
-    addWeatherToPage = () => {
-
-        if (this.state.load === true) {
-            debugger
-            let city = this.state.data.name
-            let temp = this.KtoC(this.state.data.main.temp);
-            let minTemp = this.KtoC(this.state.data.main.temp_min);
-            let maxTemp = this.KtoC(this.state.data.main.temp_max);
-            let pressure = this.pressureCout(this.state.data.main.pressure)
-
-            return (
-                <div>
-                    <div> City: {city}</div>
-                    <div> Temperature:
-                        <ul>
-                            <li>Now: {temp}°</li>
-                            <li>Min: {minTemp}°</li>
-                            <li>Max: {maxTemp}°</li>
-                        </ul>
-                        <div>Pressure: {pressure} mm Hg</div>
-                    </div>
-
-
-                </div>
-
-            )
-
-        }
-
-    }
-    pressureCout = (P) => {
-        return Math.floor(P / 1.3333)
-    }
-
-    KtoC = (K) => {
-        return Math.floor(K - 273.15);
-    }
-    inputValue = (e) => {
-        this.setState({
-            inputValue: e.currentTarget.value
-        })
-
-
-    }
-
-    render() {
-        return (
-            <div>
-                <Serch
-                    addWeatherToPage={this.addWeatherToPage}
-                    request={this.request}
-                    inputValue={this.inputValue}
-                />
-            </div>
-        )
-
-    }
+  render() {
+    return (
+      <div>
+        <Serch
+          addWeatherToPage={this.addWeatherToPage}
+          req={this.req}
+          val={this.state.inputValue}
+          inputValue={this.inputValue}
+        />
+      </div>
+    );
+  }
 }
--- src/Main/Serch.js
+++ src/Main/Serch.js
@@ -3,21 +3,17 @@ import s from '../App.module.css'
 
 
 export default class Serch extends React.Component {
-    constructor() {
-        super();
-        this.state = {}
-
-
-    }
-
+    
+   
     render() {
+        debugger
         return (
             <body className={s.App_header}>
             <div className={s.sqrt}>
                 <div><input onChange={this.props.inputValue} className={s.input} id="myInput" type={'text'} placeholder={'Enter your town!'}>
                 </input></div>
                 <div className={s.body}>
-                    <button onClick={this.props.request}>Click</button>
+                    <button onClick={e=>{this.props.req(this.props.val)}}>Click</button>
                 </div>
                 <div>{this.props.addWeatherToPage()}</div>
             </div>
